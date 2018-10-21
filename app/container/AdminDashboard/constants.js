78368d533245e3ac97f0181f7e80e12a64e09372
Budi Prakosa <iyok@deadmediafm.org>
2018-10-21 19:24:27 +0700

    add request string

--- app/container/AdminDashboard/constants.js
+++ app/container/AdminDashboard/constants.js
@@ -1,12 +1,12 @@
 
-export const FETCH_ACTIVE_USER = 'codemi/AdminDashboard/FETCH_ACTIVE_USER';
+export const FETCH_ACTIVE_USER = 'codemi/AdminDashboard/FETCH_ACTIVE_USER_REQUEST';
 export const FETCH_ACTIVE_USER_SUCCESS = 'codemi/AdminDashboard/FETCH_ACTIVE_USER_SUCCESS';
 export const FETCH_ACTIVE_USER_FAILURE = 'codemi/AdminDashboard/FETCH_ACTIVE_USER_FAILURE';
 
-export const FETCH_MOST_ACTIVE_USER = 'codemi/AdminDashboard/FETCH_MOST_ACTIVE_USER';
+export const FETCH_MOST_ACTIVE_USER = 'codemi/AdminDashboard/FETCH_MOST_ACTIVE_USER_REQUEST';
 export const FETCH_MOST_ACTIVE_USER_SUCCESS = 'codemi/AdminDashboard/FETCH_MOST_ACTIVE_USER_SUCCESS';
 export const FETCH_MOST_ACTIVE_USER_FAILURE = 'codemi/AdminDashboard/FETCH_MOST_ACTIVE_USER_FAILURE';
 
-export const FETCH_COURSE = 'codemi/AdminDashboard/FETCH_COURSE';
+export const FETCH_COURSE = 'codemi/AdminDashboard/FETCH_COURSE_REQUEST';
 export const FETCH_COURSE_SUCCESS = 'codemi/AdminDashboard/FETCH_COURSE_SUCCESS';
 export const FETCH_COURSE_FAILURE = 'codemi/AdminDashboard/FETCH_COURSE_FAILURE';
